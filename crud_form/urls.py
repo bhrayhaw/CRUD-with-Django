c00a98c0c2ece67759b253cdf8beb3a00e19086d
bhrayhaw <elijahapreko@gmail.com>
2023-09-18 11:43:52 +0000

    updated with class based view

--- crud_form/urls.py
+++ crud_form/urls.py
@@ -7,7 +7,9 @@ urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('musician_details/<pk>/', views.MusicianDetails.as_view(), name='musician_details'),
     path('create_musician/', views.CreateMusician.as_view(), name='create_musician'),
-    path('create_album/<int:artist_id>/', views.CreateAlbum.as_view(), name='create_album'),
     path('update_musician/<pk>/', views.UpdateMusician.as_view(), name='update_musician'),
     path('delete_musician/<pk>/', views.DeleteMusician.as_view(), name='delete_musician'),
+    path('create_album/<int:artist_id>/', views.CreateAlbum.as_view(), name='create_album'),
+    path('update_album/<pk>/', views.UpdateAlbum.as_view(), name='update_album'),
+    path('delete_album/<pk>/', views.DeleteAlbum.as_view(), name='delete_album'),
 ]
--- crud_form/views.py
+++ crud_form/views.py
@@ -22,17 +22,6 @@ class CreateMusician(CreateView):
     model = models.MusicianModel
     template_name = 'musician_form.html'
     
-class CreateAlbum(CreateView):
-    context_object_name = 'album'
-    model = models.AlbumModel
-    fields = ('album_name', 'release_date', 'number_stars')
-    template_name = 'album_form.html'
-
-    def form_valid(self, form):
-        form.instance.artist = MusicianModel.objects.get(id=self.kwargs['artist_id'])
-        return super().form_valid(form)
-
-
 class UpdateMusician(UpdateView):
     model = models.MusicianModel
     fields = ['first_name', 'last_name', 'email', 'instrument']
@@ -43,3 +32,22 @@ class DeleteMusician(DeleteView):
     model = models.MusicianModel
     template_name = 'delete.html'
     success_url = reverse_lazy('crud_form:index')
+class CreateAlbum(CreateView):
+    context_object_name = 'album'
+    model = models.AlbumModel
+    fields = ('album_name', 'release_date', 'number_stars')
+    template_name = 'album_form.html'
+
+    def form_valid(self, form):
+        form.instance.artist = MusicianModel.objects.get(id=self.kwargs['artist_id'])
+        return super().form_valid(form)
+class UpdateAlbum(UpdateView):
+    model = models.AlbumModel
+    fields = ['album_name', 'release_date', 'number_stars']
+    template_name = 'update_album.html'
+
+class DeleteAlbum(DeleteView):
+    context_object_name = 'album'
+    model = models.AlbumModel
+    template_name = 'delete_album.html'
+    success_url = reverse_lazy('crud_form:musician_details')
